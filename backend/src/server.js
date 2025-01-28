a38357e811dbaa1d487ab345e090c29e1bd57f45
Apurvborhade <apurvaborhadee@gmail.com>
2025-01-29 00:03:32 +0530

    cors update

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
     cors({
-        origin: 'https://eternity-dvjf.onrender.com',
+        origin: 'https://eternity-nine.vercel.app/',
         credentials: true
     })
 )

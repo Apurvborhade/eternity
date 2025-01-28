c0e44fbbbd7095582960793ed597377f83b44b7e
Apurvborhade <apurvaborhadee@gmail.com>
2025-01-28 23:36:32 +0530

    proxy

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://eternity-dvjf.onrender.com',
         credentials: true
     })
 )
